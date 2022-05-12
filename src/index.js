4cec91f4358c03b8628ec9cefda658b451728c66
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-05-12 13:48:42 -0500

    Added form validation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import './style.css';
 import { addScore, showScores } from './modules/apiRequests.js';
+import {
+  userError,
+  scoreError,
+  resetStyle,
+  showMsg,
+} from './modules/validation.js';
 
 const refreshScores = document.querySelector('.refbtn');
 const submitScore = document.querySelector('.sbmtbtn');
@@ -8,9 +14,19 @@ refreshScores.addEventListener('click', () => { showScores(); });
 
 submitScore.addEventListener('click', async (event) => {
   event.preventDefault();
+
   const user = document.getElementById('user');
   const score = document.getElementById('score');
 
-  await addScore(user.value, score.value);
-  showScores();
+  if (user.value === '') {
+    userError();
+  } else if (score.value === '') {
+    scoreError();
+  } else {
+    await addScore(user.value, score.value);
+    showScores();
+    event.target.parentElement.reset();
+    resetStyle();
+    showMsg();
+  }
 });
