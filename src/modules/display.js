3886223bf86436766804fcec94b5327150fdc217
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-05-17 16:36:03 -0500

    Update Project

--- src/modules/display.js
+++ src/modules/display.js
@@ -2,7 +2,7 @@ const displayScore = (scoreArr) => {
   const scoreList = document.querySelector('.scorelist');
   scoreList.innerHTML = null;
   scoreArr.sort((a, b) => b.score - a.score);
-  scoreArr.slice(0, 10).forEach((item) => {
+  scoreArr.forEach((item) => {
     const scoreItem = document.createElement('li');
     scoreItem.innerHTML = `<div class="scitem"><p>${item.user}</p><p>:    ${item.score}</p></div>`;
     scoreList.append(scoreItem);
